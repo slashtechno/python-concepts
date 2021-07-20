b33572aa5b7e617af3bc3d932722ddcb4ca15b02
slashtechno <77907286+slashtechno@users.noreply.github.com>
2021-07-20 12:11:17 -0500

    Create a dictionary

---
+++ dictionaries.py
@@ -0,0 +1,4 @@
+# I am following the following tutorial to learn dictionaries
+# https://www.youtube.com/watch?v=daefaLgNkw0
+
+student = {"name" : "John", "age": 25, "courses" : ["Math", "CompiSci"]}
