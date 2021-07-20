89c8a28965a2da90866a117a4b2e7d27f3d93359
slashtechno <77907286+slashtechno@users.noreply.github.com>
2021-07-20 14:17:09 -0500

    Print various information about a dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -1,6 +1,6 @@
 # I am following the following tutorial to learn dictionaries
 # https://www.youtube.com/watch?v=daefaLgNkw0
-
+# Also I add \n to some print statements to make reading the output easier
 student = {"name" : "John", "age": 25, "courses" : ["Math", "CompiSci"]}
 
 print(student) # Print the whole dictionary
@@ -19,8 +19,20 @@ student.update({"name" : "Jane", "age" : 26, "phone" : "555-555-5555"})
 # NOTE: A dictionary can also be updated with student["name"] = "Hello, world!"
 print(student)
 
-# Delete a key value
+# Delete a key's value
+print("Delete a key's value")
 del student["age"]
 student.update({"age" : 26})
 age = student.pop("age") # copy value of a key to a variable and then remove the value of the key
 print(student)
+
+# Print various information about a dictionary
+print("The number of keys in the dictionary: " + str(len(student))) # Print number of keys in a dictionary
+# Print only the keys in a dictionary line by line
+print("The keys in the dictionary")
+for key in student:
+    print (key)
+# Print the pairs of keys and values in a dictionary
+print("Pairs of keys and values in the dictionary")
+for key, value in student.items():
+    print(key, value)
